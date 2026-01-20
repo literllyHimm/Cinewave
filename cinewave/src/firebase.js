1bd75b6ba41423be4ed1d6c3e06d9befbefeaadd
arjunkotwal-del <arjun.kotwal@ontariotechu.net>
2026-01-20 19:45:14 +0000

    fix firebase error

--- cinewave/src/firebase.js
+++ cinewave/src/firebase.js
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider, setPersistence, browserSessionPersistence } from "firebase/auth";
 
-// 🔹 Firebase Configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -12,8 +11,8 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// 🔹 Initialize Firebase App
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+
 
 // 🔹 Initialize Services
 export const db = getFirestore(app);  
